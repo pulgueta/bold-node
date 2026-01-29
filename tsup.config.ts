baff9d3f095add2e1d0a9d1eb0f6e4b03f867fa5
pulgueta <retardix456@gmail.com>
2026-01-29 15:52:59 -0500

    chore: init repo

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  format: "esm",
+  clean: true,
+  minify: true,
+  platform: "node",
+  treeshake: true,
+  splitting: true,
+  minifySyntax: true,
+});
